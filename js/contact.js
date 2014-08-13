bfb2e36ea73d2ab22efaa2026443ece970764ec1
gushterche <gushterche_2006@abv.bg>
2014-08-13 17:08:50 +0300

    zoom menu

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,4 @@
-function initialize() {
+	function initialize() {
         var map_canvas = document.getElementById('map_canvas');
         var map_options = {
           center: new google.maps.LatLng(42.492381, 27.47954),
@@ -7,5 +7,7 @@ function initialize() {
         }
         var map = new google.maps.Map(map_canvas, map_options)
       	}
+      	
 
       google.maps.event.addDomListener(window, 'load', initialize);
+
